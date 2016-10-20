ba6ea7d21c628814cc4e6aceffab18098e5aa6c7
elimiller783 <emiller783@gmail.com>
2016-10-20 10:16:57 -0400

    done

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,6 +20,41 @@ class BankVault {
     
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+
+}
+
+extension BankVault: ProvideAccess {
+    
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        var weGood = false
+        
+        
+        if let unwrappedPassword = password.first{
+            let remainder = unwrappedPassword % 2
+            
+            if remainder == 0 {
+                for (index, value) in password.enumerated() {
+                    if index % 2 == 0 && value % 2 == 0 {
+                        weGood = true
+                    }
+                }
+            }
+            
+            
+        }else{
+            print(password)
+        }
+        
+        //let  remainder = password.first! % 2
+        
+        
+        return weGood
+    }
+    
+    
+}
 
 
 
